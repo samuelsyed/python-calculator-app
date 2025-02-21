94464b1272861f9bdf85c6634acbcfe5310cea90
Samuel Syed <s.syed@elsevier.com>
2025-02-21 12:53:02 +0000

    adding main guard

--- calculator.py
+++ calculator.py
@@ -27,24 +27,30 @@ def calculate(first_num,second_num,operation):
     result = operations[operation](first_num,second_num)
     return  result
 
-recent_result = []
-start = float(input("Enter your first number?: "))
-carry_on = True
+def main():
+    recent_result = []
+    start = float(input("Enter your first number?: "))
+    carry_on = True
 
 
-while carry_on:
-    operand = input('Choose an operation: "+","-", "*","/": ')
-    next_num = float(input("Enter your next num: "))
-    final_result =  calculate(first_num=start,second_num=next_num,operation=operand)
-    recent_result.append(final_result)
-    print(f"{start} {operand} {next_num} = {final_result}")
-    proceed = input(f"Type 'y' to continue calculating with {final_result} : or type 'n' to start a new calculation: ").lower()
-    if proceed == "y":
-        if len(recent_result) == 1:
-            index = 0
-        else:
-            index = -1
-        start = recent_result[-1]
-    if proceed == "n":
-        recent_result = []
-        start = float(input("Enter your first number?: "))
+    while carry_on:
+        operand = input('Choose an operation: "+","-", "*","/": ')
+        next_num = float(input("Enter your next num: "))
+        final_result =  calculate(first_num=start,second_num=next_num,operation=operand)
+        recent_result.append(final_result)
+        print(f"{start} {operand} {next_num} = {final_result}")
+        proceed = input(f"Type 'y' to continue calculating with {final_result} : or type 'n' to start a new calculation: ").lower()
+        if proceed == "y":
+            if len(recent_result) == 1:
+                index = 0
+            else:
+                index = -1
+            start = recent_result[-1]
+        if proceed == "n":
+            recent_result = []
+            start = float(input("Enter your first number?: "))
+
+
+
+if __name__ == "__main__":
+    main()
